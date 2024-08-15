586370a7fb2b8e83abe0c8ea4202edba7978f40c
Okpara007 <chinemeremokpara93@gmail.com>
2024-08-15 08:12:11 +0100

    made changes

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -3,10 +3,15 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the API, Go to '/graphql' for access interface ")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),  # No jwt_cookie decorator
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', home),  # This will handle requests to the root URL /
+    path('admin/', admin.site.urls),  # Admin panel URL
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),  
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
 ]
